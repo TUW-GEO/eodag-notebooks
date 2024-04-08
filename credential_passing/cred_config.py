c98afc121e1499c082258bffc84b67d70dafe4d4
Nikolas Pikall <nikolas.pikall@tuwien.ac.at>
2024-04-08 11:25:53 +0000

    Upload New File

---
+++ credential_passing/cred_config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 09:47:10 2024
+
+This Script showcases the useage of Environment Variables to pass Credentials 
+to EODAG.
+
+@author: npikall
+"""
+#Imports
+from eodag import EODataAccessGateway
+
+
+#%%
+#Set Credentials for Copernicus Dataspace Ecosystem (CDSE) as Environment Variable.
+dag = EODataAccessGateway()
+dag.update_providers_config("""
+    cop_dataspace:
+        download:
+            outputs_prefix:
+        auth:
+            credentials:
+                username: PLEASE_CHANGE_USERNAME
+                password: PLEASE_CHANGE_PASSWORD
+""")
+
+dag.set_preferred_provider("cop_dataspace") # Copernicus Data Space Ecosystem
