d6896d680ce7b1a4a4de79d56a3d6197f3e933d2
Nikolas Pikall <nikolas.pikall@tuwien.ac.at>
2024-05-17 17:13:32 +0200

    restructured repo

--- src/eotools/geometry.py
+++
@@ -1,8 +0,0 @@
-import rioxarray
-import geopandas as gpd
-from shapely.geometry import mapping
-
-def clip_dataset_2_shapefile(ds, shapefile):
-    clip_shape = gpd.read_file(shapefile)
-    ds = ds.rio.clip(clip_shape.geometry.apply(mapping), clip_shape.crs, drop=False, invert=False)
-    return ds
